d442892fbada4c596b1077fb80ba7508e741d35d
Sean Castanon <castanons@usf.edu>
2021-05-30 00:05:01 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using API.Data.Gov.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace API.Data.Gov.Controllers
+{
+    public class HomeController : Controller
+    {
+        HttpClient httpClient;
+
+        static string BASE_URL = "https://developer.nps.gov/api/v1";
+        static string API_KEY = "hgdtOgIopQXJnAiJfCeckCj18NWc0kYQ5dmSeYqf"; //Add your API key here inside ""
+
+
+        public IActionResult Index()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string NATIONAL_PARK_API_PATH = BASE_URL + "/parks?limit=20";
+            string parksData = "";
+
+            Parks parks = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    parksData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!parksData.Equals(""))
+                {
+                    // JsonConvert is part of the NewtonSoft.Json Nuget package
+                    parks = Newtonsoft.Json.JsonConvert.DeserializeObject<Parks>(parksData);
+                }
+
+
+            }
+            catch (System.Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+
+            return View(parks);
+        }
+    }
+}
+
+
+
+
+
